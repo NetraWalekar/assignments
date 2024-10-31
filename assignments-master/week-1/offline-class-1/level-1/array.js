c4c4dc6e8e9614735c7e141bc79978ba3a38e026
NetraWalekar <netrawalekar6@gmail.com>
2024-10-31 20:56:47 +0530

    changes

--- assignments-master/week-1/offline-class-1/level-1/array.js
+++ assignments-master/week-1/offline-class-1/level-1/array.js
@@ -8,13 +8,26 @@
 
 // console.log(newArray);
 
-const a = [1,2,3,4,5];
-const b = [6,7,8,9,0];
+// const a = [1,2,3,4,5];
+// const b = [6,7,8,9,0];
 
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+
+// let ans = a.map((item,index) => sum(item,b[index]));
+// console.log(ans);
+
+const array = [1,2,3,4,5,6,7,8,9];
+
+let newArray = [];
+
+for(let i=0;i<array.length;i++){
+if(array[i]%2==0){
+    newArray.push(array[i]);
+}
 }
 
-let ans = a.map((item,index) => sum(item,b[index]));
-console.log(ans);
+
+console.log(newArray);
     
